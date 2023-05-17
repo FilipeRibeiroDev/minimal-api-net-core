7105e44f728caf78f076bd96c7db2342ba4fcc7a
Filipe Brito <filipe.brito@hubconexa.com>
2023-05-17 07:42:19 -0300

    Primeiro Commit

---
+++ CarrinhoCompras/Program.cs
@@ -0,0 +1,51 @@
+using Microsoft.Extensions.Caching.Distributed;
+using System.Text.Json;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetValue<string>("CacheSettings:ConnectionString");
+});
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.MapPost("/carrinhos", async (Carrinho carrinho, IDistributedCache redis) =>
+{
+    await redis.SetStringAsync(carrinho.UsuarioId, JsonSerializer.Serialize(carrinho));
+    return true;
+});
+
+app.MapGet("/carrinhos/{usuarioId}", async (string usuarioId, IDistributedCache redis) =>
+{
+    var data = await redis.GetStringAsync(usuarioId);
+
+    if (string.IsNullOrEmpty(data)) return null;
+
+    var carrinho = JsonSerializer.Deserialize<Carrinho>(data, new JsonSerializerOptions
+    {
+        PropertyNameCaseInsensitive = false
+    });
+
+    return carrinho;
+});
+
+app.Run();
+
+
+record Carrinho (string UsuarioId, List<Produto> Produtos);
+
+record Produto (string Nome, int Quantidade, decimal PrecoUnitario);
